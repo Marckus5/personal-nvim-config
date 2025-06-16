26ba08b3ad83aa2d04d2a15139caea096cda7c31
Marckus5 <mav.aldrin@gmail.com>
2025-06-16 22:33:01 +0800

    code folding with ufo

--- lua/options.lua
+++ lua/options.lua
@@ -1,21 +1,30 @@
 require "nvchad.options"
 
-require('refactoring').setup()
+require("refactoring").setup()
 
--- TODO: add yours here! 
+-- TODO: add yours here!
 
-require("todo-comments")
+require "todo-comments"
 local o = vim.o
-o.cursorlineopt ='both' -- to enable cursorline!
+o.cursorlineopt = "both" -- to enable cursorline!
 
-
-local null_ls = require('null-ls')
-null_ls.setup({
-sources = {
-            null_ls.builtins.diagnostics.ruff,   -- Ruff
-            null_ls.builtins.diagnostics.mypy,   -- MyPy
-    }
-})
-
+local null_ls = require "null-ls"
+null_ls.setup {
+  sources = {
+    null_ls.builtins.diagnostics.ruff, -- Ruff
+    null_ls.builtins.diagnostics.mypy, -- MyPy
+  },
+}
 
 require("neogit").setup()
+
+-- open all folds by default
+vim.o.foldcolumn = '1' -- '0' is not bad
+vim.o.foldlevel = 99 -- Using ufo provider need a large value, feel free to decrease the value
+vim.o.foldlevelstart = 99
+vim.o.foldenable = true
+require("ufo").setup {
+  provider_selector = function(bufnr, filetype, buftype)
+    return { "treesitter", "indent" }
+  end,
+}
